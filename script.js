45c32a3d993ca20baf36278abd1c449f5e8606f1
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-01-30 14:40:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// asking user how many km he wants to travel
+
+const KmToTravel = Number( prompt("Quanti chilometri desideri percorrere?") );
+
+// asking user what's his age
+
+const Age = Number( prompt("Qual è la tua età?") );
+
+
+// Generating base price
+
+const standardPrice = 0.21 * KmToTravel;
+
+
+
+
+let finalPrice;
+
+if(Age < 18) {
+    finalPrice = standardPrice - standardPrice * 0.2;
+} else if (Age >= 65) {
+    finalPrice = standardPrice - standardPrice * 0.4;
+} else {
+    finalPrice = standardPrice
+}
+
+
+
+
+
+console.log( finalPrice );
+console.log( "€ " + finalPrice.toFixed(2) );
+
+
+
+
+document.getElementById("ticket-total-price").innerHTML = "€ " + finalPrice;
